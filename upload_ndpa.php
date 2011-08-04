37d8532cc5cbd5a8367253d442ecaf3e5272cd3a
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2011-08-04 19:35:41 -0400

    interpreting the annotations xml correctly

--- upload_ndpa.php
+++ upload_ndpa.php
@@ -10,6 +10,10 @@
 # algorithms in list key. Also returns name = None to indicate that algorithm
 # was not successfully returned
 
+# Report all PHP errors (see changelog)
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On'); 
+
 @$cmd = $_FILES["file"];
 
 if(!isset($cmd))
@@ -44,13 +48,82 @@ else
   echo "Upload: " . $_FILES["file"]["name"] . "<br />";
   echo "Type: " . $_FILES["file"]["type"] . "<br />";
   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  echo "Stored in: " . $_FILES["file"]["tmp_name"] . ",br/>";
 	
 	# Analyze the XML file and follow python code
 	try
 		{
 		$xml = simplexml_load_file($_FILES["file"]["tmp_name"]);
-		echo $xml->asXML();
+		
+		$bookmarks = array();	
+		# Browse through the bookmarks and list them
+		foreach ($xml->children() as $elem) 
+			{
+			$bookmark = array();
+			$bookmark['title'] = (string) $elem->title;
+			$bookmark['details'] = (string) $elem->details;
+			$bookmark['center'] = array( intval($elem->x), intval($elem->y), intval($elem->z));
+			$bookmark['lens'] = floatval((string)$elem->lens);
+
+			$annot  = array();
+			$points = array();
+			$apoint = array();
+
+			foreach($elem->annotation->attributes() as $name=>$value)
+				{
+					$annot[$name] = (string) $value;
+				}
+			$isx = 1;
+			
+			foreach($elem->annotation->children() as $child)
+				{
+				switch($child->getName())
+					{
+					case 'pointlist':
+						foreach($child->children() as $coords)
+							{
+							$points[] = array(intval($coords->x), intval($coords->y));
+							}
+						break;
+
+					case 'measuretype':
+						$annot['measuretype'] = intval($child);
+						break;
+
+					case 'closed':
+						$annot['closed'] = intval($child);
+						break;
+					
+					case 'radius':
+						$annot['radius'] = intval($child);
+						break;
+
+					case 'specialtype':
+						$annot['specialtype'] = (string) $child;
+						break;
+					
+					default:
+						if($isx)
+							{
+							$isx = 0;
+							$apoint[0] = intval($child);
+							}
+						else
+							{
+							$isx = 1;
+							$apoint[1] = intval($child);
+							$points[] = $apoint;
+							}
+							break;
+					}
+				}
+			$annot['points'] = $points;
+			$bookmark['annotation'] = $annot; 
+			$bookmarks[] = $bookmark;	
+			}
+		echo "<pre> ";
+		print_r($bookmarks);
+		echo "</pre>";
 		}
 	//catch exception
 	catch(Exception $e)
