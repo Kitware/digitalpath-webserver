2b46e2130af745ff96740c1095280558849eb713
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2011-08-10 13:15:38 -0400

    Correct headers

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 		<meta name = "viewport" content = "width = device-width">
     <link rel="apple-touch-icon" href="favicon.ico" />
+		<link href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" rel="stylesheet" type="text/css" />
 		<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
 		<!-- large image specific additions  -->
