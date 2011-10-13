bc283f0080653a1af7af8c2d46d4d9fb9746e9df
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2011-10-12 20:54:18 -0400

    Stop leading button changes

--- libs/mobile-jq.js
+++ libs/mobile-jq.js
@@ -552,31 +552,41 @@ function init()
 				}
 			else
 				{
-				isFollowing = true;
-				$('#follow').attr("data-theme", 'e').removeClass("ui-btn-up-"+'a').removeClass("ui-btn-hover-"+'a').addClass("ui-btn-up-"+'e');
-				$('#follow .ui-btn-text').text('Stop Session');
-				// Change theme 
-			
+
+				// if leading is true then stop leading
+				if(isLeading === true)
+					{
+					isLeading = false
+					// Change the theme back to black
+					$('#follow').attr("data-theme", 'a').removeClass("ui-btn-up-"+'b').removeClass("ui-btn-hover-"+'b').addClass("ui-btn-up-"+'a');
+					$('#follow .ui-btn-text').text('Join Session');
+						
+					}
+					else
+					{
+						isFollowing = true;
+						$('#follow').attr("data-theme", 'e').removeClass("ui-btn-up-"+'a').removeClass("ui-btn-hover-"+'a').addClass("ui-btn-up-"+'e');
+						$('#follow .ui-btn-text').text('Stop Leading');
+						// Change theme 
+					}
+				
 				}
 
 			timersec.toggle();
 		});
 
-		$("#lead").bind("vclick", function(event, ui){
+		$("#lead").bind("vclick", function(event, ui)
+			{
 			// start a timer and 
-			if(isLeading === true)
-				{
-				isLeading = false
-				}
-			else
-				{
 				isLeading = true;
 				// Register for the events 
 
-				}
+				$('#follow').attr("data-theme", 'b').removeClass("ui-btn-up-"+'a').removeClass("ui-btn-hover-"+'a').addClass("ui-btn-up-"+'b');
+				$('#follow .ui-btn-text').text('Stop Session');
+			
 			
 
-		});
+			});
 
 
 		function change_annotation_mode(evemt, ui, num)
