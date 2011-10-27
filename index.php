282325f2b007c7d739e89a63ec58accba9da7267
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2011-10-26 20:19:53 -0400

    Added facebook button, but not much useful right now

--- index.php
+++ index.php
@@ -137,6 +137,14 @@ Faulkner-Jones and Su-jean Seo. All rights reserved";
 				</form>
 
 				-->
+				
+				<!-- login -->
+					Or authenticate using :
+				<form action="?login" method="post" data-ajax="false">
+					<center>
+					<button data-theme="a" data-inline="true" data-ajax="false">Facebook</button>
+					</center>
+				</form>
 
 			</div>
 		</div>
