df904c0fe20021b99796addf54b4bc5db4d33edc
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-01-30 15:14:11 -0500

    Facebook groups information trasnferred

--- access-groups.php
+++ access-groups.php
@@ -1,54 +1,79 @@
 <?php
 	# To be called from facebook or google submodule
+	session_start();
+	#print_r($_SESSION);
+	#return;
 
-	# Check for categorical logins
-	if(isset($book) && isset($pass)) 
+	function display_user_information()
 		{
-	
-		if($book == "demo" )
-			{
-				session_start();
-				$_SESSION['book'] = 'demo';
-				$_SESSION['copyright'] = "Copyright &copy 2011, All rights reserved";
-				$_SESSION['auth'] = 'admin';
-				header("location:session-index.php");
-				return;
-			}
+		# Displays user information including group memberships 
 		
+		foreach ($_SESSION['facebook'] as $key => $val)
+			{	
+			echo $key . " " . $val . "</br>";
+			} 
 
-		if($book == "wustl" )
-			{
-			if($pass == "showme" || $pass == 'MOmanage')
-				{
-				session_start();
-				$_SESSION['book'] = 'paul2';
-				$_SESSION['copyright'] = "Copyright &copy 2011, Paul Bridgman. All rights reserved";
-				$_SESSION['auth'] = 'student';
-				if($pass == 'MOmanage')
-					{
-					$_SESSION['auth'] = 'admin';
-					}
-				header("location:session-index.php");
-				return;
-				}
-			}
-		
-		if($book == "hms")
-			{
-			if($pass == "letmein" || $pass == 'MAmanage')
-				{
-				session_start();
-				$_SESSION['book'] = 'bev1';
-				$_SESSION['copyright'] = "Copyright &copy 2011, Charles Palmer, Beverly
-Faulkner-Jones and Su-jean Seo. All rights reserved";
-				$_SESSION['auth'] = 'student';
-				if($pass == 'MAmanage')
-					{
-					$_SESSION['auth'] = 'admin';
-					}
-				header("location:session-index.php");
-				return;
-				}
-			}
+		foreach ($_SESSION['groups'] as $key => $val)
+			{	
+			echo $key . " " . $val['name'] . "</br>";
+			} 
 		}
+
+	display_user_information();
+
+	# Loads the groups database, and lists the intersection
+	# Cleans up session cookie
+
+	#Verify 
+
+#	# Check for categorical logins
+#	if(isset($book) && isset($pass)) 
+#		{
+#	
+#		if($book == "demo" )
+#			{
+#				session_start();
+#				$_SESSION['book'] = 'demo';
+#				$_SESSION['copyright'] = "Copyright &copy 2011, All rights reserved";
+#				$_SESSION['auth'] = 'admin';
+#				header("location:session-index.php");
+#				return;
+#			}
+#		
+#
+#		if($book == "wustl" )
+#			{
+#			if($pass == "showme" || $pass == 'MOmanage')
+#				{
+#				session_start();
+#				$_SESSION['book'] = 'paul2';
+#				$_SESSION['copyright'] = "Copyright &copy 2011, Paul Bridgman. All rights reserved";
+#				$_SESSION['auth'] = 'student';
+#				if($pass == 'MOmanage')
+#					{
+#					$_SESSION['auth'] = 'admin';
+#					}
+#				header("location:session-index.php");
+#				return;
+#				}
+#			}
+#		
+#		if($book == "hms")
+#			{
+#			if($pass == "letmein" || $pass == 'MAmanage')
+#				{
+#				session_start();
+#				$_SESSION['book'] = 'bev1';
+#				$_SESSION['copyright'] = "Copyright &copy 2011, Charles Palmer, Beverly
+#Faulkner-Jones and Su-jean Seo. All rights reserved";
+#				$_SESSION['auth'] = 'student';
+#				if($pass == 'MAmanage')
+#					{
+#					$_SESSION['auth'] = 'admin';
+#					}
+#				header("location:session-index.php");
+#				return;
+#				}
+#			}
+#		}
 ?>
