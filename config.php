68590718534fd8e31372a4ce4e7979e481345626
Brian Helba <brian.helba@kitware.com>
2012-02-07 14:22:15 -0500

    Change database hostname to work on slide-atlas.org

--- config.php
+++ config.php
@@ -17,17 +17,9 @@ if (!isset($_SESSION['book']))
 else
 	{
 	# define some variables
-	$server = "127.0.0.1:27017";
+	$server = "slide-atlas.org:27017";
 	$database = $_SESSION['book'];
 	$base_url = "tile.php";
-	#$server = "140.247.106.249:27017";
-	#	$server = "amber11:27017";
-#	$database = $_SESSION['book'];
-#	$base_url = "tile.php";
-
-#	$server = "ayodhya:27017";
-#	$database = $_SESSION['book'];
-#	$base_url = "http://ayodhya:82/tile.php";
 	}
 
 ?>
