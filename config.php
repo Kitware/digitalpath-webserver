6242ac9a0430f19a74f3bad75cda31bc8313bce7
Brian Helba <brian.helba@kitware.com>
2012-02-07 14:58:01 -0500

    Change Facebook return URL and database hostname to work on live servers

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 
 # The location of the configuration dialog
-$_SESSION['loginConnName'] = 'ayodhya:27017';
+$_SESSION['loginConnName'] = 'slide-atlas.org:27017';
 $_SESSION['loginDBName'] = 'slideatlas';
 
 // initialize a session
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 # Move these to index.php config file?
-$loginConnName = 'ayodhya:27017';
+$loginConnName = 'slide-atlas.org:27017';
 $loginDBName = 'slideatlas';
 
 # Logging in with Google $accounts requires setting special identity, so this example shows how to do it.
